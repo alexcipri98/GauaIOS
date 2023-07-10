ea26033e62c6870a0ed12b5b74035212c69de1bc
Alex <Alex.Ciprian.Lopez@logirail.com>
2023-07-10 12:09:31 +0200

    Initial Commit

---
+++ Gaua/GauaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GauaApp.swift
+//  Gaua
+//
+//  Created by Alex Ciprián López on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GauaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
