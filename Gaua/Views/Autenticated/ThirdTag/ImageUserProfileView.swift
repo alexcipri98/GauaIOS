0a39b5a0b0fb70b4dc6e7848174d47ae2e9a5a0b
Alex <Alex.Ciprian.Lopez@logirail.com>
2023-08-05 12:51:10 +0200

    First Step: Show data of user. Next Step: Edit data of user

---
+++ Gaua/Views/Autenticated/ThirdTag/ImageUserProfileView.swift
@@ -0,0 +1,54 @@
+//
+//  ImageUserProfileView.swift
+//  Gaua
+//
+//  Created by Alex Ciprián López on 4/8/23.
+//
+
+import SwiftUI
+
+struct ImageUserProfileView: View {
+    @EnvironmentObject var viewModel: ProfileViewModel
+
+    var body: some View {
+        let shouldPresentCropView = viewModel.isShowingCropViewController && viewModel.selectedImage != nil
+        Button(action: {
+            viewModel.isShowingImagePicker = true
+        }) {
+            ComponentStyles.customImageOfUser()
+                .frame(width: 200, height: 200)
+                .clipShape(Circle())
+                .shadow(radius: 10)
+                .overlay(
+                    Circle()
+                        .stroke(Color.white, lineWidth: 5)
+                )
+                .padding(.top, 10)
+                .overlay(
+                    Image(systemName: "plus")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.green)
+                        .padding(10)
+                        .background(Color.white)
+                        .clipShape(Circle())
+                        .offset(x: 80, y: 80)
+                )
+        }
+        .sheet(isPresented: $viewModel.isShowingImagePicker, onDismiss: viewModel.showCrop) {
+            ImagePicker(image: $viewModel.selectedImage)
+        }
+        .sheet(isPresented: Binding<Bool>(get: { shouldPresentCropView },
+                                              set: { _ in viewModel.isShowingCropViewController = false }),
+                   onDismiss: viewModel.loadImage) {
+                CropViewControllerRepresentable(viewModel: self.viewModel)
+            }
+    }
+}
+
+struct ImageUserProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageUserProfileView()
+    }
+}
--- Gaua/Views/Autenticated/ThirdTag/ProfileView.swift
+++ Gaua/Views/Autenticated/ThirdTag/ProfileView.swift
@@ -11,55 +11,20 @@ struct ProfileView: View {
     @StateObject private var viewModel = ProfileViewModel()
 
     var body: some View {
-        let shouldPresentCropView = viewModel.isShowingCropViewController && viewModel.selectedImage != nil
-        
-        VStack {
-            ZStack {
-                Button(action: {
-                    viewModel.isShowingImagePicker = true
-                }) {
-                    ComponentStyles.customDefaultUserImage()
-                        .frame(width: 200, height: 200)
-                        .clipShape(Circle())
-                        .shadow(radius: 10)
-                        .overlay(
-                            Circle()
-                                .stroke(Color.white, lineWidth: 5)
-                        )
-                        .padding(.top, 30)
-                        .overlay(
-                            Image(systemName: "plus")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 30, height: 30)
-                                .foregroundColor(.green)
-                                .padding(10)
-                                .background(Color.white)
-                                .clipShape(Circle())
-                                .offset(x: 80, y: 80)
-                        )
-                }
-                .sheet(isPresented: $viewModel.isShowingImagePicker, onDismiss: viewModel.showCrop) {
-                    ImagePicker(image: $viewModel.selectedImage)
-                }
-                .sheet(isPresented: Binding<Bool>(get: { shouldPresentCropView },
-                                                      set: { _ in viewModel.isShowingCropViewController = false }),
-                           onDismiss: viewModel.loadImage) {
-                        CropViewControllerRepresentable(viewModel: self.viewModel)
-                    }
-                
+        ScrollView() {
+            ImageUserProfileView().environmentObject(viewModel)
+            
+            VStack(alignment: .leading, spacing: 10) {
+                BodyOfProfileView()
             }
-            .frame(maxWidth: .infinity, maxHeight: 350)
-            .cornerRadius(20)
-            .padding(.horizontal, 20)
-            .padding(.bottom, 30)
-
-
-            Spacer()
         }
+        .frame(maxHeight: UIScreen.main.bounds.height * 0.8)
+        .padding(.trailing)
+        .padding(.leading)
         .navigationBarTitle("Profile")
     }
 }
+
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
